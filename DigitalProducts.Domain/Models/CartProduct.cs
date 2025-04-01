b75b4dceb651e0bdfc3c97339a17e04e17b083a1
JulioDev1 <juliocesargualberto617@gmail.com>
2025-04-01 13:32:15 -0300

    [Backend] fix the idententation

--- DigitalProducts.Domain/Models/CartProduct.cs
+++ DigitalProducts.Domain/Models/CartProduct.cs
@@ -4,9 +4,9 @@ namespace DigitalProducts.Domain.Models
 {
     public class CartProduct
     {
-        public long cartId { get; set; }
-        public long productId { get; set; }
-        public int quantity { get; set; }  
+        public long CartId { get; set; }
+        public long ProductId { get; set; }
+        public int Quantity { get; set; }  
         public required Product product { get; set; }
         public required Cart cart { get; set; }
     }
