e24b2502febdf5be3467c5dfa9e91f719817d23c
JulioDev1 <juliocesargualberto617@gmail.com>
2025-04-05 09:16:30 -0300

    [Backend] create product interface

--- DigitalProducts.Application/Commands/User/CreateUserHandler/CreateUserRequest.cs
+++ DigitalProducts.Application/Commands/User/CreateUserHandler/CreateUserRequest.cs
@@ -5,12 +5,9 @@ namespace DigitalProducts.Application.Commands.User.CreateUserHandler
 {
     public class CreateUserRequest : IRequest<long>
     {
-
-        
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public UserDto.RoleUser RoleUser { get; set; }
-
     }
 }
---
+++ DigitalProducts.Infra/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+using DigitalProducts.Domain.Models;
+
+namespace DigitalProducts.Infra.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<long> CreateProduct(Product product);
+    }
+}
